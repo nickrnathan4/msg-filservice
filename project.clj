99f49fecf43484116d91eb4fecdbb821c9e2b0b0
Nick Nathan <nrn2@georgetown.edu>
2015-01-14 16:51:56 -0500

    remove dependency spaces, update url, change uberjar back

--- project.clj
+++ project.clj
@@ -1,15 +1,11 @@
 (defproject msg-fileservice "0.1.0-SNAPSHOT"
   :description "CRUD file service backed by S3 and Datomic"
-  :url "http://example.com/FIXME"
+  :url "http://msg-fileservice.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-
-                 ;; Utilities
                  [clj-time "0.8.0"]
                  [environ "1.0.0"]
-
-                 ;;Webserver
                  [ring/ring-defaults "0.1.3"]
                  [com.palletops/leaven "0.2.1"]
                  [com.palletops/bakery-httpkit "0.2.0" :exclude [http-kit]]
@@ -17,19 +13,13 @@
                  [ring-basic-authentication "1.0.5"]
                  [fogus/ring-edn "0.2.0"]
                  [liberator "0.12.2"]
-
-                 ;; AWS
                  [clj-aws-s3 "0.3.10" :exclusions [joda-time]]
-
-                 ;; Datomic
                  [datomic-schema "1.1.0"]
-                 [com.datomic/datomic-pro "0.9.5052" :exclusions [joda-time]]
-                 ]
+                 [com.datomic/datomic-pro "0.9.5052" :exclusions [joda-time]]]
 
   :uberjar-name "msg-fileservice-standalone.jar"
   :main msg-fileservice.core
-  :profiles {:uberjar {:aot :all}
-             :production {:env {:production true}}
+  :profiles {:uberjar {:aot [msg-fileservice.core]}
              :dev {:source-paths ["dev" "src"]
                    :dependencies [[org.clojure/tools.namespace "0.2.7"]]}}
 
@@ -38,5 +28,4 @@
                   :username ~(System/getenv "DATOMIC_USERNAME")
                   :password ~(System/getenv "DATOMIC_PASSWORD")}
                  }
-
   )
